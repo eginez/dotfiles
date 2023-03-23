326f458309df7279eed163ef7aca06cfa30b764c
Esteban Ginez <175813+eginez@users.noreply.github.com>
2023-03-23 09:14:49 -0700

    fixes shortcut for open files

--- nvim-config/nvim/lua/core/keymaps.lua
+++ nvim-config/nvim/lua/core/keymaps.lua
@@ -48,7 +48,7 @@ keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window max
 keymap.set("n", "<leader>n", ":NvimTreeToggle<CR>") -- toggle file explorer
 
 -- telescope
-keymap.set("n", "<D-f>", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<D-o>", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
 keymap.set("n", "<D-g>", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
