98bb6f70c05f3443f30d1fb990e38091ee3d1869
Esteban Ginez <175813+eginez@users.noreply.github.com>
2024-02-10 22:31:32 -0800

    update install script

--- install_plugins.sh
+++ install_plugins.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
-set -u  # die on undeclared vars
+set -u          # die on undeclared vars
 set -o pipefail # die on pipe failures
-set -e #die on error
+set -e          #die on error
 
 DWL="sudo apt-get install -y vim zsh fzf jq ripgrep tree"
 ## missing ccls in linux
 ## snap install --classic cls
 
 if [[ USE_NVIM ]]; then
-  VIM=neovim
+	VIM=neovim
 else
-  VIM=macvim
+	VIM=macvim
 fi
 
 DWNL_DARWIN="brew install zsh $VIM fzf diff-so-fancy jq ripgrep tree ccls npm lua-language-server"
@@ -30,30 +30,28 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 mkdir -p ~/.vim.backup
 mkdir -p ~/.vim
 
-
 echo "Creating zshrc file"
 rm -rf ~/.zshrc
-ln -s `pwd -P`/zshrc ~/.zshrc
+ln -s $(pwd -P)/zshrc ~/.zshrc
 
 echo "Adding custom theme for zsh"
-ln -s `pwd -P`/lambda-color.zsh-theme ~/.oh-my-zsh/custom/themes/lambda-color.zsh-theme
+ln -s $(pwd -P)/lambda-color.zsh-theme ~/.oh-my-zsh/custom/themes/lambda-color.zsh-theme
 
 echo "Adding power10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-ln -s `pwd -P`/p10k.zsh ~/.p10k.zsh
+ln -s $(pwd -P)/p10k.zsh ~/.p10k.zsh
 
 echo "Adding better zsh vim mode"
 git clone https://github.com/jeffreytse/zsh-vi-mode ${ZSH:-$HOME}/custom/plugins/zsh-vi-mode
 
 echo "Creating vimrc file"
-ln -s `pwd -P`/vimrc ~/.vimrc
+ln -s $(pwd -P)/vimrc ~/.vimrc
 
 echo "Creating gitconfig file"
-ln -s `pwd -P`/gitconfig ~/.gitconfig
+ln -s $(pwd -P)/gitconfig ~/.gitconfig
 
 echo "Creating tmux config file"
-ln -s `pwd -P`/tmux.conf ~/.tmux.conf
-
+ln -s $(pwd -P)/tmux.conf ~/.tmux.conf
 
 echo "Installing fzf key integration"
 if [[ "$(uname -s)" == "Darwin" ]]; then
@@ -61,17 +59,15 @@ if [[ "$(uname -s)" == "Darwin" ]]; then
 fi
 
 if [[ USE_NVIM ]]; then
-  echo "configuring nvim"
-  ln -s `pwd -P`/nvim-config/nvim ~/.config
-  nvim +PackerSync +qall
+	echo "configuring nvim"
+	ln -s $(pwd -P)/nvim-lazy/nvim ~/.config
+	nvim +PackerSync +qall
 else
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-
-  echo "Start vim and execute :PlugInstall"
-  ## vim -u plugins.vim  +PlugInstall +qall  # Install plugins only
-  vim +PlugInstall +qall
-  vim +CocInstall coc-json coc-tsserver
+	echo "Start vim and execute :PlugInstall"
+	## vim -u plugins.vim  +PlugInstall +qall  # Install plugins only
+	vim +PlugInstall +qall
+	vim +CocInstall coc-json coc-tsserver
 fi
-
