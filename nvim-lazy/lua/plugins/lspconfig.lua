7f0a5e0c80bcfe68e98023dbd8aee3f316e695ed
Esteban Ginez <175813+eginez@users.noreply.github.com>
2024-02-10 16:41:01 -0800

    adds D-k keymap for goto decl

---
+++ nvim-lazy/lua/plugins/lspconfig.lua
@@ -0,0 +1,13 @@
+-- LSP keymaps
+return {
+  "neovim/nvim-lspconfig",
+  init = function()
+    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+    -- change a keymap
+    --keys[#keys + 1] = { "K", "<cmd>echo 'hello'<cr>" }
+    -- disable a keymap
+    --keys[#keys + 1] = { "K", false }
+    -- add a keymap
+    keys[#keys + 1] = { "<D-k>", "<cmd>lua vim.lsp.buf.declaration()<cr>" }
+  end,
+}
