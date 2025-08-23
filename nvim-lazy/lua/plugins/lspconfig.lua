a21b90db09e7abced4576c190f683dabc87fa2ba
Esteban Ginez <175813+eginez@users.noreply.github.com>
2025-08-23 14:54:29 -0700

    adding mojo

--- nvim-lazy/lua/plugins/lspconfig.lua
+++ nvim-lazy/lua/plugins/lspconfig.lua
@@ -1,3 +1,4 @@
+local util = require("lspconfig.util")
 -- LSP keymaps
 return {
   "neovim/nvim-lspconfig",
@@ -10,5 +11,28 @@ return {
     -- add a keymap
     keys[#keys + 1] = { "<D-k>", "<cmd>lua vim.lsp.buf.declaration()<cr>" }
     keys[#keys + 1] = { "<D-i>", "<cmd>lua vim.lsp.buf.references()<cr>" }
+
+    -- Add filetype detection for Mojo
+    vim.filetype.add({
+      extension = {
+        mojo = "mojo",
+        ["🔥"] = "mojo",
+      },
+    })
+  end,
+
+  opts = function(_, opts)
+    -- Ensure servers table exists
+    opts.servers = opts.servers or {}
+
+    -- Add Mojo LSP
+    opts.servers.mojo = {
+      cmd = { "mojo-lsp-server" }, -- Replace with actual command
+      filetypes = { "mojo" },
+      root_dir = function(fname)
+        return require("lspconfig.util").find_git_ancestor(fname) or vim.fn.getcwd()
+      end,
+      settings = {},
+    }
   end,
 }
